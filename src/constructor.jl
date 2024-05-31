87ee940962b9de6bb11a5fdf7677e9f2f9b95def
Olivier Cots <olivier.cots@irit.fr>
2024-05-31 17:44:19 +0200

    foo

--- src/constructor.jl
+++ src/constructor.jl
@@ -21,24 +21,6 @@ rhs(Σ::AbstractSystem, args...) 	= Σ.rhs! #throw_not_implemented("rhs", Σ)
 is_hamiltonian(Σ::AbstractSystem)   = false;
 # Fin API par défaut
 
-
-# macro _flow_function_fixed(f, _f, __variable)
-# 	q1 = :(
-# 		function $f(tspan::Tuple{Time,Time}, x0; rhs, kwargs...)
-# 			return $_f(tspan, x0, $__variable(); rhs, kwargs...)
-# 		end
-# 	)
-# 	#
-# 	q2 = :(
-# 		function $f(t0::Time, x0, tf::Time; kwargs...)
-# 			return $_f(t0, x0, tf, $__variable(); kwargs...)
-# 		end
-# 	)
-# 	#
-# 	code = Expr(:block, q1, q2)
-# 	esc(code)
-# end
-
 """
 $(TYPEDSIGNATURES)
 
@@ -75,75 +57,124 @@ function _core_flow(tspan, x0, v, rhs, alg, abstol, reltol, saveat; kwargs...)
 end
 
 let
-	n = 2
-	x = ( :(X1), )
-	for i=2:n 
-		x = (x..., Symbol("X", string(i)))
-	end
-	e_tuple = Expr(:curly, :(Tuple), x...)
-	e_call  = :( g(Σ::$e_tuple, n::Int)::Int )
-	e_where = Expr(:where, e_call, x...)
-	e_core  = quote 
-		return n
-	end
-	e_fun   = Expr(:function, e_where, e_core)
-	eval(e_fun)
-	g((1, 2), 3)
-end
 
-"""
-$(TYPEDSIGNATURES)
+	for n ∈ 2:4
 
-Return a flow from a system. It mainly constructs the caller for the flow.
-"""
-function Flow(Σ::AbstractSystem{Tuple{X1, X2}, V}, args...; alg=__alg(), abstol=__abstol(), reltol=__reltol(), 
-	saveat=__saveat(), kwargs_Flow...)::AbstractFlow where {X1, X2, V}
-	
-	# default variable from the system
-	__variable = default_variable(Σ)
-	
-	# conversion
-	convert_state    = convert_state_function(Σ)
-	convert_variable = convert_variable_function(Σ)
-	convert_ode_sol  = convert_ode_sol_function(Σ)
-	convert_ode_u    = convert_ode_u_function(Σ)
+		# tuple {X1, X2, ...}
+		x = ( Symbol("X", string(i)) for i ∈ 1:n )
+		flow_tuple = Expr(:curly, :(Tuple), x...)
 
-	# 
-	if is_variable(Σ)
+		# signature of Flow
+		flow_call =  :( 
+			Flow(Σ::AbstractSystem{$flow_tuple, V}, args...; alg=__alg(), abstol=__abstol(), reltol=__reltol(), 
+			saveat=__saveat(), kwargs_Flow...)::AbstractFlow
+		)
 
-		function f_variable(tspan::Tuple{Time,Time}, x01::X1, x02::X2, v::V=__variable(); rhs, kwargs...)
-			sol = _core_flow(tspan, convert_state(x01, x02), convert_variable(v), 
-				rhs, alg, abstol, reltol, saveat; kwargs_Flow..., kwargs...)
+		# definition of the function Flow with "where" keyword	
+		flow_where = Expr(:where, flow_call, x..., :(V))
+
+		# first part of the core of the function Flow
+		flow_core_1  = quote 
+			# default variable from the system
+			__variable = default_variable(Σ)
+			
+			# conversion
+			convert_state    = convert_state_function(Σ)
+			convert_variable = convert_variable_function(Σ)
+			convert_ode_sol  = convert_ode_sol_function(Σ)
+			convert_ode_u    = convert_ode_u_function(Σ)
+		end
+
+		#
+		X_args_f = ( Expr(:(::), Symbol(:(x0), string(i)), Symbol(:(X), string(i))) for i ∈ 1:n)
+		X_args_c =  ( Symbol(:(x0), string(i)) for i ∈ 1:n)
+
+		# f_variable 1/2
+		f_variable_call = Expr(:call, :(f_variable), 
+					Expr(:parameters, :(rhs), :(kwargs...)),
+					:(tspan::Tuple{Time,Time}), X_args_f...,
+					Expr(:kw, :(v::V), :(__variable())))
+
+		f_variable_convert_state = Expr(:call, :(convert_state), X_args_c...)
+		f_variable_core = quote 
+			sol = _core_flow(tspan, $f_variable_convert_state, convert_variable(v), 
+					rhs, alg, abstol, reltol, saveat; kwargs_Flow..., kwargs...)
 			return convert_ode_sol(sol)
 		end
+		f_variable_fun_1 = Expr(:function, f_variable_call, f_variable_core)
 
-		function f_variable(t0::Time, x01::X1, x02::X2, tf::Time, v::V=__variable(); rhs, kwargs...)
-			sol = _core_flow((t0, tf), convert_state(x01, x02), convert_variable(v), 
-				rhs, alg, abstol, reltol, saveat; kwargs_Flow..., kwargs...)
+		# f_variable 2/2
+		f_variable_call = Expr(:call, :(f_variable), 
+					Expr(:parameters, :(rhs), :(kwargs...)),
+					:(t0::Time), X_args_f..., :(tf::Time),
+					Expr(:kw, :(v::V), :(__variable())))
+
+		f_variable_convert_state = Expr(:call, :(convert_state), X_args_c...)
+		f_variable_core = quote 
+			sol = _core_flow((t0, tf), $f_variable_convert_state, convert_variable(v), 
+					rhs, alg, abstol, reltol, saveat; kwargs_Flow..., kwargs...)
 			return convert_ode_u(sol(tf))
 		end
+		f_variable_fun_2 = Expr(:function, f_variable_call, f_variable_core)
 
-	 	return _construct_flow(Σ, f_variable)
+		# f_fixed 1/2
+		f_fixed_call = Expr(:call, :(f_fixed), 
+					Expr(:parameters, :(rhs), :(kwargs...)),
+					:(tspan::Tuple{Time,Time}), X_args_f...)
 
-	else # si le système est Fixed, on ne permet pas de fournir une variable
-	
-		function f_fixed(tspan::Tuple{Time,Time}, x01::X1, x02::X2; rhs, kwargs...)
-			sol = _core_flow(tspan, convert_state(x01, x02), convert_variable(__variable()), 
-				rhs, alg, abstol, reltol, saveat; kwargs_Flow..., kwargs...)
+		f_fixed_convert_state = Expr(:call, :(convert_state), X_args_c...)
+		f_fixed_core = quote 
+			sol = _core_flow(tspan, $f_fixed_convert_state, convert_variable(__variable()), 
+					rhs, alg, abstol, reltol, saveat; kwargs_Flow..., kwargs...)
 			return convert_ode_sol(sol)
 		end
+		f_fixed_fun_1 = Expr(:function, f_fixed_call, f_fixed_core)
 
-		function f_fixed(t0::Time, x01::X1, x02::X2, tf::Time; rhs, kwargs...)
-			sol = _core_flow((t0, tf), convert_state(x01, x02), convert_variable(__variable()), 
-				rhs, alg, abstol, reltol, saveat; kwargs_Flow..., kwargs...)
+		# f_fixed 2/2
+		f_fixed_call = Expr(:call, :(f_fixed), 
+					Expr(:parameters, :(rhs), :(kwargs...)),
+					:(t0::Time), X_args_f..., :(tf::Time),)
+
+		f_fixed_convert_state = Expr(:call, :(convert_state), X_args_c...)
+		f_fixed_core = quote 
+			sol = _core_flow((t0, tf), $f_fixed_convert_state, convert_variable(__variable()), 
+					rhs, alg, abstol, reltol, saveat; kwargs_Flow..., kwargs...)
 			return convert_ode_u(sol(tf))
 		end
+		f_fixed_fun_2 = Expr(:function, f_fixed_call, f_fixed_core)
 
-		return _construct_flow(Σ, f_fixed)
+		# flow core 2
+		flow_core_2 = quote
+			if is_variable(Σ)
 
-	end
+				$f_variable_fun_1
+		
+				$f_variable_fun_2
+		
+				return _construct_flow(Σ, f_variable)
+		
+			else # si le système est Fixed, on ne permet pas de fournir une variable
+			
+				$f_fixed_fun_1
+		
+				$f_fixed_fun_2
+		
+				return _construct_flow(Σ, f_fixed)
+		
+			end
+		end
 
-end
+		# Core of the Flow function
+		flow_core = Expr(:block, flow_core_1, flow_core_2)
+
+		# Flow function
+		flow_fun  = Expr(:function, flow_where, flow_core)
+
+		#
+		eval(flow_fun)
+
+	end # end for
+end # end let
 
 """
 $(TYPEDSIGNATURES)
