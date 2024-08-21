3f84ebecfc1570a0e76fbadabbb209a2fd587967
Olivier Cots <66357348+ocots@users.noreply.github.com>
2024-08-21 18:11:00 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,13 +14,13 @@ const CTFlowsODE = Base.get_extension(CTFlows, :CTFlowsODE)
 @testset verbose = true showtiming = true "CTFlows" begin
     for name ∈ (
         :aqua,
-        # :concatenation,
-        # :default,
-        # :flow_function,
-        # :flow_hamiltonian_vector_field,
-        # :flow_hamiltonian,
-        # :flow_vector_field,
-        # :optimal_control_problem,
+        :concatenation,
+        :default,
+        :flow_function,
+        :flow_hamiltonian_vector_field,
+        :flow_hamiltonian,
+        :flow_vector_field,
+        :optimal_control_problem,
         )
         @testset "$(name)" begin
             test_name = Symbol(:test_, name)
