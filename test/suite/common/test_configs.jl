1a6c02f841628f7c2dd9b815cab7f5c642c1fcd9
Olivier Cots <olivier.cots@irit.fr>
2026-05-05 11:54:27 +0200

    Add comprehensive tests for config hierarchy
    - Add tests for AbstractPointConfig and AbstractTrajectoryConfig exports
    - Add subtype tests for HamiltonianPointConfig and HamiltonianTrajectoryConfig
    - Add construction tests for Hamiltonian configs
    - Add tspan tests for Hamiltonian configs
    - Add display tests for Hamiltonian configs
    - All 84 tests passing

--- test/suite/common/test_configs.jl
+++ test/suite/common/test_configs.jl
@@ -83,16 +83,48 @@ function test_configs()
                 Test.@test isdefined(Common, :AbstractConfig)
             end
 
+            Test.@testset "AbstractPointConfig is exported" begin
+                Test.@test isdefined(Common, :AbstractPointConfig)
+            end
+
+            Test.@testset "AbstractTrajectoryConfig is exported" begin
+                Test.@test isdefined(Common, :AbstractTrajectoryConfig)
+            end
+
             Test.@testset "PointConfig subtypes AbstractConfig" begin
                 config = Common.PointConfig(0.0, [1.0], 1.0)
                 Test.@test config isa Common.AbstractConfig
                 Test.@test Common.PointConfig <: Common.AbstractConfig
             end
 
+            Test.@testset "PointConfig subtypes AbstractPointConfig" begin
+                config = Common.PointConfig(0.0, [1.0], 1.0)
+                Test.@test config isa Common.AbstractPointConfig
+                Test.@test Common.PointConfig <: Common.AbstractPointConfig
+            end
+
             Test.@testset "TrajectoryConfig subtypes AbstractConfig" begin
                 config = Common.TrajectoryConfig((0.0, 1.0), [1.0])
                 Test.@test config isa Common.AbstractConfig
                 Test.@test Common.TrajectoryConfig <: Common.AbstractConfig
+            end
+
+            Test.@testset "TrajectoryConfig subtypes AbstractTrajectoryConfig" begin
+                config = Common.TrajectoryConfig((0.0, 1.0), [1.0])
+                Test.@test config isa Common.AbstractTrajectoryConfig
+                Test.@test Common.TrajectoryConfig <: Common.AbstractTrajectoryConfig
+            end
+
+            Test.@testset "HamiltonianPointConfig subtypes AbstractPointConfig" begin
+                config = Common.HamiltonianPointConfig(0.0, [1.0], [0.5], 1.0)
+                Test.@test config isa Common.AbstractPointConfig
+                Test.@test Common.HamiltonianPointConfig <: Common.AbstractPointConfig
+            end
+
+            Test.@testset "HamiltonianTrajectoryConfig subtypes AbstractTrajectoryConfig" begin
+                config = Common.HamiltonianTrajectoryConfig((0.0, 1.0), [1.0], [0.5])
+                Test.@test config isa Common.AbstractTrajectoryConfig
+                Test.@test Common.HamiltonianTrajectoryConfig <: Common.AbstractTrajectoryConfig
             end
         end
 
@@ -115,6 +147,23 @@ function test_configs()
                 Test.@test config.tspan == (0.0, 1.0)
                 Test.@test config.x0 == [1.0, 0.0]
             end
+
+            Test.@testset "HamiltonianPointConfig construction" begin
+                config = Common.HamiltonianPointConfig(0.0, [1.0, 0.0], [0.5, 0.3], 1.0)
+                Test.@test config isa Common.HamiltonianPointConfig
+                Test.@test config.t0 === 0.0
+                Test.@test config.x0 == [1.0, 0.0]
+                Test.@test config.p0 == [0.5, 0.3]
+                Test.@test config.tf === 1.0
+            end
+
+            Test.@testset "HamiltonianTrajectoryConfig construction" begin
+                config = Common.HamiltonianTrajectoryConfig((0.0, 1.0), [1.0, 0.0], [0.5, 0.3])
+                Test.@test config isa Common.HamiltonianTrajectoryConfig
+                Test.@test config.tspan == (0.0, 1.0)
+                Test.@test config.x0 == [1.0, 0.0]
+                Test.@test config.p0 == [0.5, 0.3]
+            end
         end
 
         # ====================================================================
@@ -136,6 +185,20 @@ function test_configs()
 
             Test.@testset "TrajectoryConfig tspan returns tuple" begin
                 config = Common.TrajectoryConfig((0.0, 1.0), [1.0, 0.0])
+                ts = Common.tspan(config)
+                Test.@test ts isa Tuple{Real, Real}
+                Test.@test ts == (0.0, 1.0)
+            end
+
+            Test.@testset "HamiltonianPointConfig tspan returns tuple" begin
+                config = Common.HamiltonianPointConfig(0.0, [1.0], [0.5], 1.0)
+                ts = Common.tspan(config)
+                Test.@test ts isa Tuple{Real, Real}
+                Test.@test ts == (0.0, 1.0)
+            end
+
+            Test.@testset "HamiltonianTrajectoryConfig tspan returns tuple" begin
+                config = Common.HamiltonianTrajectoryConfig((0.0, 1.0), [1.0], [0.5])
                 ts = Common.tspan(config)
                 Test.@test ts isa Tuple{Real, Real}
                 Test.@test ts == (0.0, 1.0)
@@ -193,6 +256,52 @@ function test_configs()
                 Test.@test occursin("TrajectoryConfig", output)
                 Test.@test occursin("tspan:", output)
                 Test.@test occursin("x0:", output)
+            end
+
+            Test.@testset "HamiltonianPointConfig show methods" begin
+                config = Common.HamiltonianPointConfig(0.0, [1.0], [0.5], 1.0)
+                io = IOBuffer()
+                show(io, config)
+                output = String(take!(io))
+                Test.@test occursin("HamiltonianPointConfig", output)
+                Test.@test occursin("t0:", output)
+                Test.@test occursin("x0:", output)
+                Test.@test occursin("p0:", output)
+                Test.@test occursin("tf:", output)
+            end
+
+            Test.@testset "HamiltonianPointConfig text/plain show method" begin
+                config = Common.HamiltonianPointConfig(0.0, [1.0], [0.5], 1.0)
+                io = IOBuffer()
+                show(io, MIME("text/plain"), config)
+                output = String(take!(io))
+                Test.@test occursin("HamiltonianPointConfig", output)
+                Test.@test occursin("t0:", output)
+                Test.@test occursin("x0:", output)
+                Test.@test occursin("p0:", output)
+                Test.@test occursin("tf:", output)
+            end
+
+            Test.@testset "HamiltonianTrajectoryConfig show methods" begin
+                config = Common.HamiltonianTrajectoryConfig((0.0, 1.0), [1.0], [0.5])
+                io = IOBuffer()
+                show(io, config)
+                output = String(take!(io))
+                Test.@test occursin("HamiltonianTrajectoryConfig", output)
+                Test.@test occursin("tspan:", output)
+                Test.@test occursin("x0:", output)
+                Test.@test occursin("p0:", output)
+            end
+
+            Test.@testset "HamiltonianTrajectoryConfig text/plain show method" begin
+                config = Common.HamiltonianTrajectoryConfig((0.0, 1.0), [1.0], [0.5])
+                io = IOBuffer()
+                show(io, MIME("text/plain"), config)
+                output = String(take!(io))
+                Test.@test occursin("HamiltonianTrajectoryConfig", output)
+                Test.@test occursin("tspan:", output)
+                Test.@test occursin("x0:", output)
+                Test.@test occursin("p0:", output)
             end
         end
     end
